97587c42a37d5811c54f3252655515d101cadc36
Manoj B M <manojbm497@gmail.com>
2025-08-05 15:15:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const express = require("express");
 
 const app = express();
-const mongoose = require("mongoose");
-main().catch(err => console.log(err));
 
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
-}
 app.get('/', (req,res) => {
     res.send("root is working");
 });
